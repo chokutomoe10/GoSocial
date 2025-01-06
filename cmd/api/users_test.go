60e4cffb242af8211990887dbd06f59b8801f333
Mikhael Bani Ardhanajaya <mikhaelbani7@gmail.com>
2025-01-06 09:37:17 +0700

    feat: release please action

--- cmd/api/users_test.go
+++ cmd/api/users_test.go
@@ -37,4 +37,27 @@ func TestGetUser(t *testing.T) {
 
 		checkResponseCode(t, http.StatusOK, rr.Code)
 	})
+
+	// t.Run("should hit the cache first and if not exists it sets the user on the cache", func(t *testing.T) {
+	// 	mockCacheStore := app.cacheStorage.Users.(*cache.MockUserStore)
+
+	// 	mockCacheStore.On("Get", int64(41)).Return(nil, nil)
+	// 	mockCacheStore.On("Get", int64(1)).Return(nil, nil)
+	// 	mockCacheStore.On("Set", mock.Anything, mock.Anything).Return(nil)
+
+	// 	req, err := http.NewRequest(http.MethodGet, "/v1/users/41", nil)
+	// 	if err != nil {
+	// 		t.Fatal(err)
+	// 	}
+
+	// 	req.Header.Set("Authorization", "Bearer "+testoken)
+
+	// 	rr := executeRequest(req, mux)
+
+	// 	checkResponseCode(t, http.StatusOK, rr.Code)
+
+	// 	mockCacheStore.AssertNumberOfCalls(t, "Get", 1)
+
+	// 	mockCacheStore.Calls = nil
+	// })
 }
